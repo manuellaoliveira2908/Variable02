22ebff8dcd2c07e1869df3d9910e02a0fe8c8034
ALUNO <ALUNO@10.85.198.12>
2024-06-04 16:31:55 -0300

    Commit project Variable02

---
+++ src/Variables02.java
@@ -0,0 +1,18 @@
+public class Variables02 {
+
+    public static void main(String[] args) {
+        //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT
+        //Step 1) Change the print statements in lines 12 and 16 so that they print the value of x.
+        //Step 2) Change the value of x to "kitty".
+        String x = "Kitty";
+        String y = "puppy";
+        String z = "bunny";
+        System.out.println(y);                    //prints puppy
+        System.out.println(y);                    //prints puppy 
+        System.out.println(x);                    //prints kitty
+        System.out.println(x);                    //prints kitty
+        System.out.println(x);                    //prints kitty
+        System.out.println(z);                    //prints bunny
+ 
+    }
+}
